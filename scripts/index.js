215f90dfe2dc2505ca85efa9d86ef3989d4555e4
kimoncar <kimoncar@yandex.ru>
2024-05-31 07:35:09 +0300

    Fix createCard and removeCard index.js

--- scripts/index.js
+++ scripts/index.js
@@ -6,26 +6,25 @@ const content = document.querySelector('.content');
 const placesList = content.querySelector('.places__list');
 
 // Функция создания карточки
-function addPlaceCard(name, link, removeCallback) {
+function createCard(name, link, removeCallback) {
   const placeCard = placeCardTemplate.querySelector('.places__item').cloneNode(true);
   const removeButton = placeCard.querySelector('.card__delete-button');
 
   placeCard.querySelector('.card__image').setAttribute('src', link);
   placeCard.querySelector('.card__image').setAttribute('alt', `Фотография места из региона: ${name}`);
   placeCard.querySelector('.card__title').textContent = name;
-  placesList.append(placeCard);
+  
+  removeButton.addEventListener('click', removeCallback);
 
-  removeButton.addEventListener('click', function(evt) {
-    removeCallback(evt.target.parentElement);
-  });
+  return placeCard;
 };
 
 // Функция удаления карточки
-function removePlaceCard(placeCard) {
-  placeCard.remove();
+function removeCard(placeCard) {
+  placeCard.target.parentElement.remove();
 };
 
 // Вывести карточки на страницу
 initialCards.forEach(function(itemCard) {
-  addPlaceCard(itemCard.name, itemCard.link, removePlaceCard);
+  placesList.append(createCard(itemCard.name, itemCard.link, removeCard));
 });
