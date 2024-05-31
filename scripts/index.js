659dd5fb51363255016d6b4a2ab451e0e033cd48
kimoncar <kimoncar@yandex.ru>
2024-05-31 07:50:38 +0300

    Fix double find DOM-elements index.js

--- scripts/index.js
+++ scripts/index.js
@@ -8,12 +8,13 @@ const placesList = content.querySelector('.places__list');
 // Функция создания карточки
 function createCard(name, link, removeCallback) {
   const placeCard = placeCardTemplate.querySelector('.places__item').cloneNode(true);
+  const cardImage = placeCard.querySelector('.card__image');
+  const cardTitle = placeCard.querySelector('.card__title');
   const removeButton = placeCard.querySelector('.card__delete-button');
 
-  placeCard.querySelector('.card__image').setAttribute('src', link);
-  placeCard.querySelector('.card__image').setAttribute('alt', `Фотография места из региона: ${name}`);
-  placeCard.querySelector('.card__title').textContent = name;
-  
+  cardImage.src = link;
+  cardImage.alt = `Фотография места из региона: ${name}`;
+  cardTitle.textContent = name;
   removeButton.addEventListener('click', removeCallback);
 
   return placeCard;
