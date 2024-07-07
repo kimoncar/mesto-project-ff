53cdc5e4479f428998b19e68cc10dcfd23d80bdb
kimoncar <kimoncar@yandex.ru>
2024-07-07 14:18:47 +0300

    Add validation forms functional in index.js

--- src/index.js
+++ src/index.js
@@ -89,3 +89,64 @@ formProfileEdit.addEventListener('submit', profileFormSubmit);
 
 // Добавление карточки
 formAddCard.addEventListener('submit', addCardFormSubmit);
+
+// Валидация форм
+const showError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add('popup__input_type_error');
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add('form__input-error_active');
+};
+
+const hideError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove('popup__input_type_error');
+  errorElement.textContent = '';
+  errorElement.classList.remove('form__input-error_active');
+};
+
+const checkInputValidity = (formElement, inputElement) => {
+  if(!inputElement.validity.valid) {
+    showError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideError(formElement, inputElement);
+  }
+}
+
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+      return !inputElement.validity.valid;
+  });
+};
+
+const toggleButton = (inputList, buttonElement) => {
+  if(hasInvalidInput(inputList)) {
+      buttonElement.classList.add('popup__button_inactive');
+  } else {
+      buttonElement.classList.remove('popup__button_inactive');
+  };
+};
+
+const setEventListeners = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
+  const buttonElement = formElement.querySelector('.popup__button');
+  toggleButton(inputList, buttonElement);
+  inputList.forEach((inputElement) => {
+      inputElement.addEventListener('input', function() {
+          checkInputValidity(formElement, inputElement);
+          toggleButton(inputList, buttonElement);
+      });
+  });
+};
+
+const enableValidation = () => {  
+  const formList = Array.from(document.querySelectorAll('.popup__form'));
+  formList.forEach((formElement) => {
+    formElement.addEventListener('submit', function(evt) {
+        evt.preventDefault();
+    });
+    setEventListeners(formElement);
+  });
+};
+
+enableValidation();
